46cf2a849d6445e0d9c87f77edb66ee59fd9d716
Allexis-Developer <alexlmangabeira@gmail.com>
2021-01-30 18:50:25 -0300

    Class ContaCorrente

---
+++ chaptertwo/chaptertwo/src/chaptertwo/exercicioone/ContaCorrente.java
@@ -0,0 +1,40 @@
+package chaptertwo.exercicioone;
+
+public class ContaCorrente {
+	
+	private Long agencia;
+	private String conta;
+	private double saldo;
+	private String nomeCliente;
+	
+	public ContaCorrente() { }
+	
+	public ContaCorrente(Long agencia, String conta, double saldo, String nomeCliente) {
+		this.agencia = agencia;
+		this.conta = conta;
+		this.saldo = saldo;
+		this.nomeCliente = nomeCliente;
+	}
+	
+	public int sacar(double valor) {
+		
+		if(valor > this.saldo) {
+			return 0;
+		}
+		this.saldo = this.saldo - valor;
+		return 1;
+	}
+	
+	public void depositar(double valor) {
+		this.saldo = this.saldo + valor;
+	}
+	
+	public void imprimir() {
+		
+		System.out.println("Agencia..........: " + this.agencia);
+		System.out.println("Conta............: " + this.conta);
+		System.out.println("Saldo............: " + this.saldo);
+		System.out.println("Nome do Cliente..: " + this.nomeCliente);
+		
+	}
+}
